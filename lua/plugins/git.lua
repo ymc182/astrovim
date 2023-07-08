ccb90b847d39fee1f496839ea087423e14cae47e
ymc182 <ymc182@gmail.com>
2023-07-09 01:56:00 +1000

    differ plug

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,9 +14,28 @@ return {
     },
     worktrees = vim.g.git_worktrees,
   },
+  -- {
+  --   'tribela/vim-transparent',
+  --   enabled= true,
+  --   lazy = false
+  -- }
   {
-    'tribela/vim-transparent',
-    enabled= true,
-    lazy = false
-  }
+  "xiyaowong/nvim-transparent",
+    lazy = false,
+    enabled = true,
+    opts = {
+      groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+  },
+  extra_groups = {
+        "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+    "NvimTreeNormal" -- NvimTree
+      }, -- table: additional groups that should be cleared
+  exclude_groups = {}, -- table: groups you don't want to clear
+    }
+  
+  },
 }
