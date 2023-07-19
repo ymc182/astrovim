066bca82f1efc42bfe0dadc6b4d9d28384b79be0
ymc182 <ymc182@gmail.com>
2023-07-20 01:11:16 +1000

    leap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -28,11 +28,12 @@ return {
     'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
     'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
     'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer','NeoTreeNormal','NeoTreeNormalNC', 'NvimTreeNormal', 'NvimTreeNormalNC',
   },
   extra_groups = {
         "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-    "NvimTreeNormal" -- NvimTree
+    "NvimTreeNormal", -- NvimTree,
+        "NvimTreeNormalNC" -- NvimTree,
       }, -- table: additional groups that should be cleared
   exclude_groups = {}, -- table: groups you don't want to clear
     },
